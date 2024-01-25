81f6ef8382244cef0db25461e2102de147985167
Diana Dvoryak <dianadvoryak.w@gmail.com>
2024-01-25 22:09:57 +0200

    fix show

--- resources/views/admin/posts/show.blade.php
+++ resources/views/admin/posts/show.blade.php
@@ -8,10 +8,23 @@
                 <a href="{{ route('admin.post.index') }}" class="icon-link mb-2">
                     back to posts
                 </a>
-
-                    <div class="feature col bg-body-tertiary rounded-3 border p-5 mb-2">
-                        <h3 class="fs-2 text-body-emphasis">{{ $post->id }}. {{ $post->title }}</h3>
+                <div class="feature col bg-body-tertiary rounded-3 border p-5 mb-2">
+                    <h3 class="fs-2 text-body-emphasis">{{ $post->id }}. {{ $post->title }}</h3>
+                    <p>Content: {{ $post->content }}</p>
+                    <p>Published: {{ $post->published === true ? 'true' : 'false' }}</p>
+                    <p>Category:
+                        @foreach($categories as $category)
+                            @if($category->id === $post->category_id)
+                                {{ $category->title }}
+                            @endif
+                        @endforeach
+                    </p>
+                    <div class="w-50 mb-2">
+                        @if(isset($post->image))
+                            <img src="{{ url('storage/' . $post->image) }}" alt="image" class="w-50">
+                        @endif
                     </div>
+                </div>
             </div>
         </div>
     </div>
